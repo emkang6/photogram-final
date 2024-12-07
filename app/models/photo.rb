b45c6fbdcdabdec200fb4c65440a655134056bd6
emkang6 <hkang6@uchicago.edu>
2024-12-07 05:01:05 +0000

    photo associations complete

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,4 +12,21 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+  validates(:poster, { :presence => true })
+
+# Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
+  has_many  :likes, class_name: "Like", foreign_key: "photo_id", dependent: :destroy
+
+# Photo#comments: returns rows from the comments table associated to this photo by the photo_id column
+  has_many  :comments, class_name: "Comment", foreign_key: "photo_id", dependent: :destroy
+
+# Photo#poster: returns a row from the users table associated to this photo by the owner_id column
+  belongs_to :poster, required: true, class_name: "User", foreign_key: "owner_id"
+
+##Indirect
+
+# Photo#fans: returns rows from the users table associated to this photo through its likes
+
+  has_many :fans, through: :likes, source: :fan
+
 end
