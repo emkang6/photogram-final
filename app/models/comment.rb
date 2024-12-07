eff4d426ff775b17ff5b98cfba4e4157114f1c0c
emkang6 <hkang6@uchicago.edu>
2024-12-07 05:42:14 +0000

    comment, like association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,16 @@
 #  photo_id   :integer
 #
 class Comment < ApplicationRecord
+
+  validates(:commenter, { :presence => true })
+
+## Direct associations
+
+# Comment#commenter: returns a row from the users table associated to this comment by the author_id column
+  belongs_to :photo, required: true, class_name: "Photo", foreign_key: "photo_id", counter_cache: true
+
+  # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
+  belongs_to :commenter, required: true, class_name: "User", foreign_key: "author_id"
+
+
 end
