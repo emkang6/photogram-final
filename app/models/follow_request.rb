339bae7e0e913e96a85b3d126c86d77045dc1b7a
emkang6 <hkang6@uchicago.edu>
2024-12-09 01:52:35 +0000

    users index homepage

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -10,4 +10,26 @@
 #  sender_id    :integer
 #
 class FollowRequest < ApplicationRecord
+
+  validates(:sender, { :presence => true})
+  validates(:recipient, { :presence => true })
+  validates(:recipient_id, {
+    :uniqueness => { :scope => [:sender_id] }
+  })
+
+    ## Direct associations
+
+  # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
+
+  belongs_to :sender, required: true, class_name: "User", foreign_key: "sender_id"
+
+  # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
+
+  belongs_to :sender, required: true, class_name: "User", foreign_key: "sender_id"
+
+  #Scope
+
+  scope :request_accepted, -> {where(status: "accepted")}
+
+
 end
