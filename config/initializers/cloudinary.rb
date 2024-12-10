bbdd11f1cc0a81f149aa3c15fa540bb97b70ce75
emkang6 <hkang6@uchicago.edu>
2024-12-10 01:20:37 +0000

    cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = ENV["dsd5zafkw"]
+  config.api_key = ENV["369453782419217"]
+  config.api_secret = ENV["9WRz-9WRz-ZC7saciIUL9CAg1Z8vk_Qc"]
+  config.cdn_subdomain = true
+end
